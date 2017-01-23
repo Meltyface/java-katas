29843b8701abf43462fb4e692dd3429ee119b827
Nathaniel Walmsley <Nathaniel Walmsley>
2017-01-23 11:57:24 +0000

    Solutions for tests 1-3

---
+++ src/main/java/com/kinbiko/java/kata/Kata2.java
@@ -0,0 +1,7 @@
+package com.kinbiko.java.kata;
+
+public class Kata2 extends Kata1 {
+	public String getMyProperty() {
+		return "overriding getMyProperty";
+	}
+}
---
+++ src/main/java/com/kinbiko/java/kata/Kata3.java
@@ -0,0 +1,10 @@
+package com.kinbiko.java.kata;
+
+public class Kata3 implements InvocationCounter {
+	public static int count = 0;
+	
+	public int getTwiceTimesInvoked() {
+		count += 1;
+		return 2*count;
+	}
+}
